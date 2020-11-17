99b781cadae0ce0d2cb2f7f90e94cd2ad62c52be
Ricardo Maya <rickmaya@gmail.com>
2020-11-17 14:55:40 -0600

    Use hooks useRef and useEffect to focus the textarea on creation

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import { ItemWrapper } from './style.js';
 
 const Item = ({ type, content }) => {
+  const textBox = React.useRef();
+
+  React.useEffect(() => {
+    textBox.current.focus();
+  });
+
   return (
     <ItemWrapper>
       {!type ? (
         <textarea
+          ref={textBox}
           value={content}
         />
       ): <p>Some new item</p>}
